4768fb903601fcbe4162c4175ffa2eeb27c19a0f
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-09-03 19:00:36 +0530

    added towers_hanoi_problem.py

---
+++ 5th-Semester/AI-LAB/Towers_Hanoi_problem/towers_hanoi_problem.py
@@ -0,0 +1,10 @@
+def towers_of_hanoi(n,source,auxiliary,destination):
+    if n==1:
+        print("Move disk 1 from",source,"to",destination)
+        return
+    towers_of_hanoi(n-1,source,destination,auxiliary)
+    print("Move disk",n,"from",source,"to",destination)
+    towers_of_hanoi(n-1,auxiliary,source,destination)
+
+n = 3
+towers_of_hanoi(n,'A','B','C')
