d664e6f195fc486de5e76265aaef2722afd28467
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-10-13 22:03:59 +0530

    added programs in AI

---
+++ 5th-Semester/AI-LAB/BFS&DFS/bfs&dfs.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+# DFS
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    if node not in visited:
+        print(node, end=' ')
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+# BFS
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extend(graph[node])
+
+print("DFS traversal starting from node A:")
+dfs(graph, 'A')
+print("\nBFS traversal starting from node A:")
+bfs(graph, 'A')
