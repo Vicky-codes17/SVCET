96f9cbdf1418695be78b8d2e08ed7f654ab6c27d
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-10-23 21:19:58 +0530

    modified Pratice in C

--- 1st-Semester/Pratice-C_Programming/Arrays/2D_array/2d_array.c
+++ 1st-Semester/Pratice-C_Programming/Arrays/2D_array/2d_array.c
@@ -3,6 +3,15 @@
 int main() {
     int a[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
+    //Size of 2D array
+    printf("Size of 2D array: %d\n", sizeof(a));
+    //Looping
+    for(int i=0;i<2;i++) {
+        for (int j=0;j<3;j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
 
     //Changing the Values 
     a[0][0] = 10;
