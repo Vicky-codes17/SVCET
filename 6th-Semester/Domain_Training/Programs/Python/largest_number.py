2f11bda6d222dfbdf00d3194c269eb554af5050c
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-12-18 12:12:43 +0530

    Mod: largest_number in practice file

--- 6th-Semester/Domain_Training/Programs/Python/largest_number.py
+++ 6th-Semester/Domain_Training/Programs/Python/largest_number.py
@@ -1,6 +1,6 @@
 ## Find the second largest number in the list
 list1 = [23,12,55,88,22,14]
-list1.sort(reverse= True)
+list1.sort(reverse= True)  # 88, 55, 23, 22, 14, 12
 print(list1[1])
 
 # Smallest number in list
