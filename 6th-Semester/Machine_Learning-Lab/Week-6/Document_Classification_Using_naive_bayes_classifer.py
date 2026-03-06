09aef797451f3e5b4a046ebd43a640d1def086e6
Vignesh R <153487240+Vicky-codes17@users.noreply.github.com>
2026-03-06 08:01:42 +0530

    Update Document_Classification_Using_naive_bayes_classifer.py

--- 6th-Semester/Machine_Learning-Lab/Week-6/Document_Classification_Using_naive_bayes_classifer.py
+++ 6th-Semester/Machine_Learning-Lab/Week-6/Document_Classification_Using_naive_bayes_classifer.py
@@ -12,7 +12,6 @@ X_vec = vectorizer.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(
     X_vec, y, test_size=0.3, random_state=42, stratify=y
 )
-
 model = MultinomialNB()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
