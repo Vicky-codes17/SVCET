ce33197ff015d2f9c864910b128fe71b3c3dcd8b
Vignesh R <rameshvignesh0706@gmail.com>
2026-03-19 15:42:45 +0530

    Added: Week-7/Cryptography/Blowfish_encryption

---
+++ 6th-Semester/Cryptography_Network_Security/program_7/Blowfish_encryption/BlowfishCipher.java
@@ -0,0 +1,30 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Scanner;
+
+public class BlowfishCipher {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter message: ");
+        String message = sc.nextLine();
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
+        SecretKey key = keyGen.generateKey();
+
+        Cipher cipher = Cipher.getInstance("Blowfish");
+
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encrypted = cipher.doFinal(message.getBytes());
+
+        System.out.println("Encrypted text: " + new String(encrypted));
+
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decrypted = cipher.doFinal(encrypted);
+
+        System.out.println("Decrypted text: " + new String(decrypted));
+    }
+}
